b802f75e74bf330c052d0b3b7ebaae4c6e3586b5
Joel Whitney <jwhitney@esri.com>
2017-03-06 10:30:36 -0800

    some clean-up

--- URLer/MainViewController.swift
+++ URLer/MainViewController.swift
@@ -21,13 +21,5 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
-    // MARK: - Navigation
-    
-    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
-        dismiss(animated: true, completion: nil)
-    }
-    
 }
 
